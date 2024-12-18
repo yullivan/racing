3891e3c84951bad1e26f29fc9632a2896b961e0a
Yul Jeon <dalbitstream@gmail.com>
2024-12-18 10:20:02 +0900

    feat: 이름 길이 검증 로직 추가, 재입력 받기(재귀recursion 사용)

--- src/main/java/racing/UserInterface.java
+++ src/main/java/racing/UserInterface.java
@@ -12,9 +12,18 @@ public class UserInterface {
     }
 
     public static List<String> getNames() {
-        String s = scanner.nextLine();
-        String[] split = s.split(",");
+        List<String> names = List.of(scanner.nextLine().split(","));
+        try {
+            for (String name : names) {
+                if (name.length() > 10) {
+                    throw new IllegalArgumentException("[ERROR] 이름은 10자 이내");
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            names = getNames();
+        }
 
-        return List.of(split);
+        return names;
     }
 }
