e829e222269b943e18815c5acb53b7d4c3fd70ee
Yul Jeon <dalbitstream@gmail.com>
2024-12-18 11:07:34 +0900

    refactor: CarName 클래스를 레코드로 변경

--- src/main/java/racing/CarName.java
+++ src/main/java/racing/CarName.java
@@ -1,39 +1,44 @@
 package racing;
 
-import java.util.Objects;
-
-public class CarName {
-
-    private String name;
-
-    public CarName(String name) {
+//public class CarName {
+//
+//    private String name;
+//
+//    public CarName(String name) {
+//        if (name.length() > 10) {
+//            throw new IllegalArgumentException("[ERROR] 자동차 이름은 10자 이내");
+//        }
+//        this.name = name;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        CarName carName = (CarName) o;
+//        return Objects.equals(name, carName.name);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hashCode(name);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "CarName{" +
+//                "name='" + name + '\'' +
+//                '}';
+//    }
+//}
+public record CarName(String name) {
+    public CarName {
         if (name.length() > 10) {
             throw new IllegalArgumentException("[ERROR] 자동차 이름은 10자 이내");
         }
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CarName carName = (CarName) o;
-        return Objects.equals(name, carName.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(name);
-    }
-
-    @Override
-    public String toString() {
-        return "CarName{" +
-                "name='" + name + '\'' +
-                '}';
     }
 }
