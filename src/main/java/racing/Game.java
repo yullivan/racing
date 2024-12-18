6c04f1a7e8c98bea3ff6a28839efcf988a3d5fa8
Yul Jeon <dalbitstream@gmail.com>
2024-12-18 11:20:28 +0900

    feat: 랜덤 숫자 함수 Game 클래스에 추가

--- src/main/java/racing/Game.java
+++ src/main/java/racing/Game.java
@@ -1,11 +1,19 @@
 package racing;
 
 import java.util.List;
+import java.util.Random;
 
 public class Game {
 
+    Random random = new Random();
+
     public void run() {
         List<CarName> carNames = UserInterface.getCarNames();
+
         System.out.println("carNames = " + carNames);
+    }
+
+    public int getRandomNumber() {
+        return random.nextInt(6) + 1;
     }
 }
